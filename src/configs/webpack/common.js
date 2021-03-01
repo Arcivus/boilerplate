3e1a1ae295c9af6c4d71ce1f7f4a5a4637376a1d
Sergey Kozhurkin <arcivus@gmail.com>
2021-03-01 15:52:28 +0300

    handle fonts files

--- src/configs/webpack/common.js
+++ src/configs/webpack/common.js
@@ -36,6 +36,17 @@ module.exports = {
           'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
           'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
       }
     ]
   },
